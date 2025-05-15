6910da7b497f5dcf1d3486bf3be865f659870d35
hanapiko <apikojuma94@gmail.com>
2025-05-16 01:45:54 +0300

    Broadcast message to connected clients

--- backend/main.go
+++ backend/main.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"net/http"
+	"sync"
+
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {return true},
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+var clients = make(map[*websocket.Conn]bool) // Track connected clients
+var mutex = sync.Mutex{}
+
+func broadcast(msg []byte) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	for client := range clients {
+		client.WriteMessage(websocket.TextMessage, msg)
+	}
 }
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request){
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		return
 	}
 	defer conn.Close()
 
+	// Add client to map
+	mutex.Lock()
+	clients[conn] = true
+	mutex.Unlock()
+
+	// Remove client when they disconnect
+	defer func() {
+		mutex.Lock()
+		delete(clients, conn)
+		mutex.Unlock()
+	}()
+
 	for {
-		msgType, msg, err := conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		conn.WriteMessage(msgType, msg)
+		broadcast(msg) // Send to all clients
 	}
 
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/ws", handleWebSocket)
 	http.ListenAndServe(":8080", nil)
 }
